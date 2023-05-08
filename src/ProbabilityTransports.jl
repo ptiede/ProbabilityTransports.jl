9ee9ccc28315bf331122c0135f87a9574dd5a357
Paul Tiede <ptiede91@gmail.com>
2023-05-08 04:43:33 -0700

    start transport stuff

--- src/ProbabilityTransports.jl
+++ src/ProbabilityTransports.jl
@@ -2,4 +2,24 @@ module ProbabilityTransports
 
 # Write your package code here.
 
+abstract type Transport end
+
+struct BoundaryTransport{S,E,T}
+    start::S
+    stop::E
+    transform::T
+end
+
+
+struct TVTransport{E, T}
+    stop::E
+    transform::T
+end
+
+
+function transform_and_logdensity(t::TVTransport, x)
+    y, lj = TV.transform_and_logjac(t, x)
+    return logpdf(d, y) + lj
+end
+
 end
