4237caa8e7f31fcc2dd6a69ccf1eb3d165e9c240
Documenter.jl <documenter@juliadocs.github.io>
2023-05-08 11:46:14 +0000

    build based on 9ee9ccc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ProbabilityTransports","category":"page"},{"location":"#ProbabilityTransports","page":"Home","title":"ProbabilityTransports","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ProbabilityTransports.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ProbabilityTransports]","category":"page"}]
+}
